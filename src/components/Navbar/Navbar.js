d652e6b0cb9d9fb6a188d1647eab10abcd71bc95
mmiguez77 <martinmiguez77@gmail.com>
2021-12-14 02:47:58 -0300

    Agregado de img

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,9 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container px-5">
-                <Link className="navbar-brand title" to="index">getonboard Jr</Link>
+                <Link className="navbar-brand title" to="/">
+                    <img src="/img/logo.png" alt="logo" className="logo"/>
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 li_menu_item">
@@ -16,6 +18,7 @@ const Navbar = () => {
 
                         <li className="nav-item"><Link className="nav-link" to="#">Iniciar sesion</Link></li>
                         <li className="nav-item"><Link className="nav-link" to="#">Registrarse</Link></li> 
+                       
                     </ul>
                 </div>
             </div>
--- src/components/Sections/FeaturesSection/FeaturesSection.js
+++ src/components/Sections/FeaturesSection/FeaturesSection.js
@@ -5,55 +5,31 @@ const FeaturesSection = () => {
     return (
         <section className="py-5" id="features">
             <div className="container px-5 my-5">
-                <div className="row">
-                    <div className="col-md-8 mb-5 mb-lg-0">
-                        <h2 className="fw-bolder mb-0">Nuestras categorías más buscadas.</h2>
-                    </div>
-                    <div class="container mt-5">
-                        <div class="row justify-content-evenly">
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
+                <div className="row justify-content-evenly"> 
+                        <div className="col-sm-12 col-md-6">
+                            <div className="col-md-6 mb-5 mb-lg-0">
+                                <h1 className="fw-bolder mb-0">Nuestras categorías más buscadas.</h1>
                             </div>
                         </div>
-                   
-                    
-                        <div class="row justify-content-around">
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
+                        <div className="col-sm-12 col-md-6">
+                            <div className="row justify-content-center">
+                                <div className="col-sm-12 col-md-4 mb-4">
+                                    <Pill titulo="Frontend"/>
+                                </div>
+                                <div className="col-sm-12 col-md-4 mb-4">
+                                    <Pill titulo="Backend"/>
+                                </div>
+                                <div className="col-sm-12 col-md-4 mb-4">
+                                    <Pill titulo="React Js"/>
+                                </div>
+                                <div className="col-sm-12 col-md-4 mb-4">
+                                    <Pill titulo="Python"/>
+                                </div>
+                                <div className="col-sm-12 col-md-4 mb-4">
+                                    <Pill titulo="HTML"/>
+                                </div>
                             </div>
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                        </div>
-
-                        <div class="row justify-content-evenly">
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                            <div class="col-sm-6 my-3 col-md-3">
-                                <Pill titulo="Frontend"/>
-                            </div>
-                        </div>
-                        
-                    </div> 
+                        </div> 
                 </div>
             </div>
         </section>
