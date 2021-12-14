3580ee946aeb7c787f26c2bfd3b8ab3eb9081017
vsol <vsol.sil@gmail.com>
2021-12-14 08:31:04 -0300

    pill cards ready

--- src/components/Cards/PillCard.js
+++ src/components/Cards/PillCard.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const PillCard = () => {
+
+const PillCard = ({link, icon, title, quantity}) => {
     return (
-        <div>
-            
+        <div class="col-lg-4 col-sm-6">
+            <Link exact to={link} className='profile__link'>
+                <div class="pillCard mb-2 mb-md-5 py-3">
+                    <div class="content">
+                        <div class="row">
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <div class="icon-big text-center">
+                                    <div class="notif">{quantity}</div>
+                                    {icon}
+                                </div>
+                            </div>
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <div class="numbers">
+                                    <p className='p-2'>{title}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Link>
         </div>
     )
 }
