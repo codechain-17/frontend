7fb31c21c26fee9ab1dab6b3e83d76ee19c823ad
vsol <vsol.sil@gmail.com>
2021-12-14 19:06:07 -0300

    changing styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({children}) => {
     return (
-        <div className="d-flex flex-column h-100">
+        <div className="layout">
             <div className="flex-shrink-0">
                 {children}
             </div>   
