d37f70c2d9c92625816f535aa83b159348eef7d8
vsol <vsol.sil@gmail.com>
2021-12-14 17:45:14 -0300

    home job cards

--- src/data/getData.js
+++ src/data/getData.js
@@ -9,3 +9,8 @@ export const companyData = async (id) => {
     const res = await axios.get(`https://www.getonbrd.com/api/v0/companies/${id}`)
     return res.data
 }
+
+export const jobsDataPerPage = async (page) => {
+    const res = await axios.get(`https://www.getonbrd.com/api/v0/search/jobs?query=%22Sin%20experiencia%22&per_page=10&page=${page}`)
+    return res.data
+}
