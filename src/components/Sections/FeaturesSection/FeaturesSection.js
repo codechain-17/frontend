8730c3a5d9c2509f47270ba628a5696b942eeca1
vsol <vsol.sil@gmail.com>
2021-12-14 18:24:57 -0300

    styles updated

--- src/components/Sections/FeaturesSection/FeaturesSection.js
+++ src/components/Sections/FeaturesSection/FeaturesSection.js
@@ -6,26 +6,26 @@ const FeaturesSection = () => {
         <section className="py-5" id="features">
             <div className="container px-5 my-5">
                 <div className="row justify-content-evenly"> 
-                        <div className="col-sm-12 col-md-6">
+                        <div className="col-sm-12 col-md-4">
                             <div className="col-md-6 mb-5 mb-lg-0">
-                                <h1 className="fw-bolder mb-0">Nuestras categorías más buscadas.</h1>
+                                <h1 className="fw-bolder mb-0 text-right">Nuestras categorías más buscadas.</h1>
                             </div>
                         </div>
-                        <div className="col-sm-12 col-md-6">
-                            <div className="row justify-content-center">
-                                <div className="col-sm-12 col-md-4 mb-4">
+                        <div className="col-sm-12 col-md-8">
+                            <div className="d-flex justify-content-evently flex-wrap">
+                                <div className="mb-4">
                                     <Pill titulo="Frontend"/>
                                 </div>
-                                <div className="col-sm-12 col-md-4 mb-4">
+                                <div className="mb-4">
                                     <Pill titulo="Backend"/>
                                 </div>
-                                <div className="col-sm-12 col-md-4 mb-4">
+                                <div className="mb-4">
                                     <Pill titulo="React Js"/>
                                 </div>
-                                <div className="col-sm-12 col-md-4 mb-4">
+                                <div className="mb-4">
                                     <Pill titulo="Python"/>
                                 </div>
-                                <div className="col-sm-12 col-md-4 mb-4">
+                                <div className="mb-4">
                                     <Pill titulo="HTML"/>
                                 </div>
                             </div>
