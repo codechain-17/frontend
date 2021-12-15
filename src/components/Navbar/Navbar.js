4c21bfc39f542c4627ad0974f34fc35a34a95a0c
vsol <vsol.sil@gmail.com>
2021-12-15 01:00:53 -0300

    ultimas integraciones componentes Lucas

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const [dataUser, setDataUser] = useState([]);
+
+    useEffect(() => {
+      fetch("/api/getuser")
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+        .catch((err) => {
+          console.log(`error: ${err}`);
+        });
+    }, []);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container px-5">
@@ -12,8 +23,8 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 li_menu_item">
                         <li className="nav-item"><Link className="nav-link" to={{pathname: "https://www.getonbrd.com/blog/posts" }} target="_blank">Blog</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/contacto">Contacto</Link></li>
-                        <li className="nav-item"><Link className="nav-link" to="/login">Iniciar sesion</Link></li>
+                       {/* {!dataUser.name && <li className="nav-item"><Link className="nav-link" to="/login">Iniciar sesion</Link></li>}
+                         {dataUser.name && <li className="nav-item"><Link className="nav-link" to={`/dashboard/${dataUser[_id]}/perfil`}>Mi perfil</Link></li>} */}
                     </ul>
                 </div>
             </div>
