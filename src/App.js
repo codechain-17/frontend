0739bf734d9f1d3cd0c982a9c1bad7330757ad34
vsol <vsol.sil@gmail.com>
2021-12-14 21:13:28 -0300

    cleaning code

--- src/App.js
+++ src/App.js
@@ -1,91 +1,75 @@
-
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './styles/App.scss';
-import Layout from './components/Layout/Layout';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import HeaderAbout from './components/Header/HeaderAbout';
-import FeaturesSection from './components/Sections/FeaturesSection/FeaturesSection';
-import TestimonialSection from './components/Sections/TestimonialSection/TestimonialSection';
-import SuscribeSection from './components/Sections/SuscribeSection/SuscribeSection';
-import AboutSection from './components/Sections/AboutSection/AboutSection';
-import BlogHome from './components/Blog/BlogHome/BlogHome';
-import Profile from './components/Profiles/Profile';
-import { SocialMediaNav } from './components/Sections/SocialMedias/SocialMediaNav';
-import Dashboard from './components/Profiles/UserProfile/Dashboard';
-import ProfileData from './components/Profiles/UserProfile/ProfileData';
-import BussinessProfile from './components/Profiles/JobProfile/BussinessProfile';
-import SearchResults from './components/Search/SearchResults';
-//import BlogPreviewSection from './components/Sections/BlogPreviewSection/BlogPreviewSection';
-//import ContactSection from './components/Sections/ContactSection/ContactSection';
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./styles/App.scss";
+import Layout from "./components/Layout/Layout";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import FeaturesSection from "./components/Sections/FeaturesSection/FeaturesSection";
+import TestimonialSection from "./components/Sections/TestimonialSection/TestimonialSection";
+import SuscribeSection from "./components/Sections/SuscribeSection/SuscribeSection";
+import ContactSection from "./components/Sections/ContactSection/ContactSection";
+import Profile from "./components/Profiles/Profile";
+import { SocialMediaNav } from "./components/Sections/SocialMedias/SocialMediaNav";
+import Dashboard from "./components/Profiles/UserProfile/Dashboard";
+import ProfileData from "./components/Profiles/UserProfile/ProfileData";
+import BussinessProfile from "./components/Profiles/JobProfile/BussinessProfile";
+import SearchResults from "./components/Search/SearchResults";
+import Postulaciones from "./components/Profiles/UserProfile/Postulaciones";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Navbar/>
-                <Switch>
-                    <Route exact path='/'>
-                        <Header options={'home'}/>
-                        <FeaturesSection/>
-                        <TestimonialSection/>
-                        <SearchResults/>
-                        <SocialMediaNav/>
-                        <SuscribeSection/>
-                    </Route>
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Header options={"home"} />
+            <FeaturesSection />
+            <TestimonialSection />
+            <SearchResults />
+            <SocialMediaNav />
+            <SuscribeSection />
+          </Route>
 
+          <Route exact path="/contacto">
+            <Header options={"contact"} />
+            <ContactSection />
+          </Route>
 
-                    <Route exact path='/contacto'>
-                        <HeaderAbout options={'about'} />
-                        <AboutSection />
-                    </Route>
+          <Route exact path="/login"></Route>
 
-                    <Route exact path='/login'>
-                       
-                    </Route>
+          <Route exact path="/dashboard/:id">
+            <Profile>
+              <Dashboard />
+            </Profile>
+          </Route>
 
-                    <Route exact path='/blog'>
-                        <BlogHome />
-                    </Route>
+          <Route exact path="/dashboard/:id/perfil">
+            <Profile>
+              <ProfileData />
+            </Profile>
+          </Route>
 
-                    <Route exact path='/dashboard/:id'>
-                        <Profile>
-                            <Dashboard />
-                        </Profile>
-                    </Route>
+          <Route exact path="/dashboard/:id/postulaciones">
+            <Profile>
+              <Postulaciones />
+            </Profile>
+          </Route>
 
-                    <Route exact path='/dashboard/:id/perfil'>
-                        <Profile>
-                            <ProfileData/>  
-                        </Profile>
-                    </Route>
+          <Route exact path="/dashboard/:id/guardado">
+            <Profile>{/* Listado de sus favoritos */}</Profile>
+          </Route>
 
-                    <Route exact path='/dashboard/:id/postulaciones'>
-                        <Profile>
-                            {/* Listado de sus postulaciones */}
-                        </Profile>
-                    </Route>
-
-                    <Route exact path='/dashboard/:id/guardado'>
-                        <Profile>
-                            {/* Listado de sus favoritos */}
-                        </Profile>
-                    </Route>
-
-                    <Route exact path='/jobs/:id'>
-                        <Profile>
-                            <BussinessProfile/>
-                        </Profile>
-                    </Route>
-
-                </Switch>
-                <Footer/>
-            </Layout>
-        </BrowserRouter>
-      
-    );
+          <Route exact path="/jobs/:id">
+            <Profile>
+              <BussinessProfile />
+            </Profile>
+          </Route>
+        </Switch>
+        <Footer />
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import headerOptions from './headerOptions';
 
 
 const Header = ({options}) => {
+    const location = useLocation();
+    console.log(location.pathname)
+
+    
     return (
         <header className="bg-dark py-5 bg" style={{backgroundImage:'url(/img/bk_home.png)'}}>
             <div className="container px-5">
@@ -14,10 +19,13 @@ const Header = ({options}) => {
                     <div className="my-5 text-center text-xl-start">
                             <h1 className="display-3 fw-bolder text-white mb-2">{headerOptions[options].title}</h1>
                             <p className="lead fw-normal text-white-50 mb-4">{headerOptions[options].description}</p>
-                            <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                <a className="btn btn-primary btn-lg px-4 me-sm-3 text-light" href="/signin">{headerOptions[options].primaryButton}</a>
-                                <a className="btn btn-outline-light btn-lg px-4 text-light border-white" href="/singup">{headerOptions[options].secondaryButton}</a>
-                            </div>
+                            {
+                                (location.pathname === '/') &&
+                                <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                                    <a className="btn btn-primary btn-lg px-4 me-sm-3 text-light" href="/signin">{headerOptions[options].primaryButton}</a>
+                                    <a className="btn btn-outline-light btn-lg px-4 text-light border-white" href="/singup">{headerOptions[options].secondaryButton}</a>
+                                </div>
+                            }
                         </div>
                     </div>
                 </div>
